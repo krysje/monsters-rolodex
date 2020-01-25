137d1ca4c8b6b04b8d42593202a338726904773a
Aman Bansiwal <amanbansiwal@gmail.com>
2020-01-26 02:10:30 +0530

    Updates

---
+++ precache-manifest.a4d41ce90b28291bdfde26827c78caa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20147316227d931c51b426e34801027f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "800be338b13ce1ac7f7f",
+    "url": "/monsters-rolodex/static/css/main.c110c503.chunk.css"
+  },
+  {
+    "revision": "0193fd501bbadc1438a6",
+    "url": "/monsters-rolodex/static/js/2.c9114c39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c9114c39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "800be338b13ce1ac7f7f",
+    "url": "/monsters-rolodex/static/js/main.eff40f0a.chunk.js"
+  },
+  {
+    "revision": "3f3d6d342e9146ee7290",
+    "url": "/monsters-rolodex/static/js/runtime-main.458d8590.js"
+  }
+]);
